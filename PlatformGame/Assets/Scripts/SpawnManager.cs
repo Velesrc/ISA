9077902b7901032340aaab5da3f78d7bbf69a709
Nick <velesrc@gmail.com>
2017-11-20 09:47:37 -0800

    modify part 3

---
+++ PlatformGame/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+	//private 
+	private Vector2 randomPosition;
+	private GameObject MyPlat, MyCoin;
+	public float lowestY;
+	//Public
+	public int blockCount = 10;
+	public GameObject coin;
+	public GameObject block;
+	public GameObject saw;
+	public int SawCount = 3;
+
+	public float HorMin = 10, HorMax = 14;
+	public float VerMin = -2, VerMax = 2;
+	// Use this for initialization
+	public Vector2 origPos; 
+	void Start () {
+		lowestY = -30;
+		origPos = new Vector2 (0f, 0f);;
+		Spawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawn(){
+		for (int i = 0; i < blockCount; i++) {
+			randomPosition = origPos + new Vector2 (Random.Range(HorMin, HorMax), Random.Range(VerMin, VerMax));
+
+			MonoBehaviour.Instantiate (coin, randomPosition + new Vector2(0f, 2), Quaternion.identity);
+			MonoBehaviour.Instantiate (saw, randomPosition + new Vector2(Random.Range(-3,3), 0f), Quaternion.identity);
+			//MyCoin.transform.parent = GameObject.Find ("Coins").transform;
+			MonoBehaviour.Instantiate (block, randomPosition, Quaternion.identity);
+			//MyPlat.transform.parent = GameObject.Find("Platforms").transform;
+			origPos = randomPosition;
+
+		}
+	}
+}
--- Unit3Modify/Assets/Scripts/PlayerController.cs
+++ Unit3Modify/Assets/Scripts/PlayerController.cs
@@ -1,79 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
 public class PlayerController : MonoBehaviour {
-
-	[HideInInspector] public bool facindRight = true;
-	[HideInInspector] public bool jump = false;
-	[SerializeField] private int coints;
-	public Text mtext;
-	public float moveForce = 365f;
-	public float maxSpeed = 5f;
-	public float jumpForce = 300f;
-	public Transform groundCheck;
-
-	private bool grounded = false;
-	private Animator anim;
+	//PRIVATE
+	private bool facingRight;
+	private UnityEngine.UI.Text SetText;
 	private Rigidbody2D rb2d;
-	private float Timer;
-	void Awake(){
-		Timer = 30;
-		anim = gameObject.GetComponent<Animator> ();
-		rb2d = gameObject.GetComponent<Rigidbody2D> ();
-
-	}
+	private SpawnManager sm;
+	// Temporary
+	private float tmpHor;
+	private Vector3 tmpPosition;
+	public float showY;
+	//PUBLIC
+	public float Timer = 30;
+	public int PlayerScore = 0;
+	public Transform groundCheck;
+	public bool jump = false;
+	public  float moveForce = 60;
+	public float maxSpeed = 5f;
+	public float jumpForce = 400f;
+	public bool grounded = false;
 	// Use this for initialization
-	void Start () {
-		mtext = GameObject.Find ("TextO").GetComponent<Text>();
+	void Awake () {
+		
+		sm = GameObject.Find ("SpawnManager").GetComponent<SpawnManager> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+		SetText = GameObject.Find("Score").GetComponent<UnityEngine.UI.Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Timer -= Time.deltaTime;
-		if (Timer <= 0 && coints < 10) {
-			mtext.text = "YOU LOSE!";
+		showY = gameObject.transform.position.y;
+		if (sm.lowestY > gameObject.transform.position.y) {
+			SetText.text = "You fall!!!";
 			Time.timeScale = 0.0f;
 		}
-		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+		Timer -= Time.deltaTime;
+		if (Timer <= 0 && PlayerScore < 10) {
+			SetText.text = "You Lose :(";
+		}
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
-		if (Input.GetButtonDown ("Jump") && grounded) {
-			jump = true;
+		if(Input.GetButtonDown("Jump") && grounded){
+				jump = true;
+			}
 		}
 
-
-	}
-
-	void FixedUpdate() {
-		float h = Input.GetAxis ("Horizontal");
-		anim.SetFloat ("Speed", Mathf.Abs (h));
-
-		if (h * rb2d.velocity.x < maxSpeed) 
-			rb2d.AddForce (Vector2.right * h * moveForce);
-
-		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
-			rb2d.velocity = new Vector2 (Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-		if (h > 0 && !facindRight)
-			Flip ();
-		else if (h < 0 && facindRight)
-			Flip ();
-		if (jump) {
-			anim.SetTrigger ("Jump");
-			rb2d.AddForce (new Vector2 (0f, jumpForce));
-			jump = false;
-		}
+	void FixedUpdate(){
+		tmpHor = Input.GetAxis ("Horizontal");
 		
-	}
+		if (tmpHor > 0 && facingRight)
+			Flip ();
+		else if (tmpHor < 0 && !facingRight)
+			Flip ();
 
-	void Flip() {
-		facindRight = !facindRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
+		if (rb2d.velocity.x * tmpHor < maxSpeed)
+			rb2d.AddForce (Vector2.right * tmpHor * moveForce);
+								
+		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed) {
+			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+		if (jump) {
+				rb2d.AddForce(new Vector2 (0f, jumpForce));
+				jump = false;
+		}
+				
+		}
+	}
+	void Flip(){
+		facingRight = !facingRight;
+		tmpPosition = transform.localScale;
+		tmpPosition.x *= -1;
+		gameObject.transform.localScale = tmpPosition;
 	}
 
 	void OnCollisionEnter2D(Collision2D other){
-		if (other.gameObject.tag == "coin")
-			coints++;
+		if (other.gameObject.tag == "coin") {
+			PlayerScore++;
+			SetText.text = "Score: " + PlayerScore.ToString ();
+
+			if (PlayerScore == 10) {
+				Time.timeScale = 0.0f;
+				SetText.text = "You WIN!!!!!";
+			}
+		} else if (other.gameObject.tag == "damage") {
+			gameObject.transform.Rotate (0f, 0f, 90);
+			GameObject.Find ("Main Camera").transform.Rotate (0f, 0f, -90);
+			Time.timeScale = 0.0f;
+			SetText.text = "YOU DIED";
+		}
 	}
 }
